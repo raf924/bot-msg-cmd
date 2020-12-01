2d42a6a547b283cfbfcf7a5d4a2de9b1b07db47a
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2020-12-01 20:11:57 +0100

    feat: add tell and whisper commands

---
+++ internal/pkg/tell.go
@@ -0,0 +1,63 @@
+package pkg
+
+import (
+	"fmt"
+	"github.com/raf924/bot/api/messages"
+	"github.com/raf924/bot/pkg/bot"
+	"github.com/raf924/bot/pkg/bot/command"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"strings"
+)
+
+func init() {
+	bot.HandleCommand(&TellCommand{private: true})
+	bot.HandleCommand(&TellCommand{private: false})
+}
+
+type TellCommand struct {
+	command.NoOpInterceptor
+	bot     command.Executor
+	private bool
+}
+
+func (t *TellCommand) Init(bot command.Executor) error {
+	t.bot = bot
+	return nil
+}
+
+func (t *TellCommand) Name() string {
+	if t.private {
+		return "whisper"
+	}
+	return "tell"
+}
+
+func (t *TellCommand) Aliases() []string {
+	return nil
+}
+
+func (t *TellCommand) Execute(command *messages.CommandPacket) ([]*messages.BotPacket, error) {
+	to := strings.TrimSpace(command.GetArgs()[0])
+	argString := command.GetArgString()
+	text := strings.TrimSpace(strings.TrimPrefix(argString, to))
+	actualTo := strings.Trim(to, "@")
+	recipient, exists := t.bot.OnlineUsers()[actualTo]
+	if !exists {
+		return []*messages.BotPacket{
+			{
+				Timestamp: timestamppb.Now(),
+				Message:   fmt.Sprintf("%s isn't online", actualTo),
+				Recipient: command.GetUser(),
+				Private:   command.GetPrivate(),
+			},
+		}, nil
+	}
+	return []*messages.BotPacket{
+		{
+			Timestamp: timestamppb.Now(),
+			Message:   text,
+			Recipient: &recipient,
+			Private:   t.private,
+		},
+	}, nil
+}
